fa5c4c1a36f1fccdeafe473cb29d9208d29f282b
more0210 <michael.ore.dev@gmail.com>
2025-01-14 22:09:36 -0500

    added images and working on auth

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,6 +20,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -31,5 +33,6 @@ export default function RootLayout({ children }) {
         </div>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
